605e6cf4b4fdc469735e142191dcbf7106c95470
Angy Hidalgo <angyhidalgo4toa@gmail.com>
2026-07-29 17:04:33 -0400

    Nuevas funciones agregadas

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Hola desde Git");
+
+function saludar(nombre){
+    return "Hola" + nombre;
+}
+
+function sumar(a, b){
+    return a+b;
+}
+
+function restar(a,b){
+    return a-b; 
+}
